e86071d8892185723898a1a2f19cae2f092fb91d
minhquan18 <lekimminhquan@gmail.com>
2024-03-18 22:57:47 +0700

    update

--- Moveonthing/Agileproject/MOT/urls.py
+++ Moveonthing/Agileproject/MOT/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.Agile, name= 'Agile' ),
+    path('',views.Agile, name= 'Agile'),
 ]
--- Moveonthing/Agileproject/MOT/views.py
+++ Moveonthing/Agileproject/MOT/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def Agile (request):
-    return (HttpResponse('hello world'))
+    template = loader.get_template('agile.html')
+    return HttpResponse(template.render())
