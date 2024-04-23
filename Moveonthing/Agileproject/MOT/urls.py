5240b6701a101294cbc25ae2883897615ca0c691
hungSF <lehung201105@gmail.com>
2024-04-23 12:16:57 +0700

    STUD-9:Update Html/css

--- Moveonthing/Agileproject/MOT/urls.py
+++ Moveonthing/Agileproject/MOT/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.Agile, name= 'Agile'),
     path('forgot',views.Forgot, name='Forgot'),
+     path('reset',views.Resetpass, name='Reset'),
     path('login', views.login_view, name= 'Login'),
     path('homepage', views.Homepage, name='Homepage'),
 ]
