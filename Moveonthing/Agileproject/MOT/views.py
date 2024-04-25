e8f5a3b09bb1002ca45337fd8e12016a513fca7f
hungSF <lehung201105@gmail.com>
2024-04-25 23:23:21 +0700

    STUD-9: complete forgot password page

--- Moveonthing/Agileproject/MOT/views.py
+++ Moveonthing/Agileproject/MOT/views.py
@@ -5,14 +5,24 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from MOT.models import Taikhoan
+from django.contrib.auth.hashers import make_password
 
 def Agile (request):
     template = loader.get_template('agile.html')
     return HttpResponse(template.render())
 
 def Forgot(request):
-    template = loader.get_template('forgotPassword.html')
-    return HttpResponse(template.render())
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        user_exists = Taikhoan.objects.filter(username=username).exists()
+        if user_exists:
+            request.session['reset_username'] = username
+            return redirect('Reset')
+        else:
+            messages.error(request, "Username does not exist.")  
+            return render(request, 'forgotPassword.html')
+    else:
+        return render(request, 'forgotPassword.html')
 
 @login_required(login_url='/login')
 def Homepage(request):
@@ -20,8 +30,32 @@ def Homepage(request):
     return HttpResponse(template.render())
 
 def Resetpass(request):
-    template = loader.get_template('resetPassword.html')
-    return HttpResponse(template.render())
+    if request.method == 'POST':
+        username = request.session.get('reset_username', '').strip()
+        new_password = request.POST.get('password', '').strip()
+        confirm_password = request.POST.get('confirm-pw', '').strip()
+        if len(new_password) > 50:
+            messages.error(request, "Password is too long")
+            return render(request, 'resetPassword.html')
+        if new_password != confirm_password:
+            messages.error(request, "Password doesn't match")
+            return render(request, 'resetPassword.html')
+        if not username:
+            messages.error(request, "Username doesn't exist, input again")
+            return redirect('ForgotPassword')
+        try:
+            user = Taikhoan.objects.get(username=username)
+            user.password = new_password 
+            user.save()
+            del request.session['reset_username']
+            messages.success(request, "Updated password")
+            return redirect('Login') 
+        except Taikhoan.DoesNotExist:
+            messages.error(request, "Username doesn't exist")
+            return render(request, 'resetPassword.html')
+
+    else:
+        return render(request, 'resetPassword.html')
 
 def Login(request):
     if request.method == 'POST':
